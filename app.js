afb8a573f4f552aeff4a94986e8c240362fa0d89
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-08-03 23:05:32 +0900

    VanllaJS Study

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "Jin"
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b); 
+
+console.log("hello " + myName);
+
+myName = "SangHun"
+console.log("my new name is " + myName);
