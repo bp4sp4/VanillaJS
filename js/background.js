ece2b1b906435da7cbd061360d6b49b6ea02ed2a
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-08-11 17:38:07 +0900

    css add

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "2.avif", "3.jpg", "4.jpg"];
+const images = ["0.jpg", "1.jpeg", "2.jpg","3.jpg", "4.avif"];
 // chosenImage에 랜덤한 이미지 노출
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 // bgImage에 img태그 생성
@@ -7,3 +7,5 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 // 바디에 appendChild이용해 사진 노출
 document.body.appendChild(bgImage);
+
+bgImage.id = 'bgImage';
