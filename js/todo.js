8403a2af00bfcc4c6e2e5341d8ff4ddb4b154d2f
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-08-11 16:17:44 +0900

    todo 완성 weather.js add

--- js/todo.js
+++ js/todo.js
@@ -2,25 +2,28 @@ const toDoForm = document.getElementById("todo-form"); // id가 todo-form인 for
 const toDoInput = document.querySelector("#todo-form input");  // id가 todo-form에 속한 input 태그
 const toDoList = document.getElementById("todo-list"); // id가 todo-list 인 ul 태그
 
-const toDos = [];
+const TODOS_KEY = "todos"
 
+let toDos = [];
 
-function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+function saveTodosToLocalStorage() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 // 버튼 눌러서 삭제함수
 function deleteToDo(event) {
     const li = event.target.parentElement; // 이벤트타겟의 부모객체를 찾음
     li.remove(); // 생성된 todolist li태그를 삭제함
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id)); // 클릭한 li.id와 다른 toDo는 남김
+    saveTodosToLocalStorage();
 }
 
-
-// toDO를 그리는 함수
-function paintToDo(newTodo){
+// toDo를 그리는 함수
+function paintToDo(newTodo) {
     const li = document.createElement("li"); // li태그 생성
+    li.id = newTodo.id;
     const span = document.createElement("span"); // span태그 생성
-    span.innerText = newTodo; // span에 newTodo 값 노출
+    span.innerText = newTodo.text; // span에 newTodo 값 노출
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -34,9 +37,20 @@ function handleToDoSubmit(event) {
     event.preventDefault(); // 입력했을때 새로고침 방지
     const newTodo = toDoInput.value; // input에서 얻은 값을
     toDoInput.value = ""; // 입력했을때 빈값을 노출
-    toDos.push(newTodo);
-    paintToDo(newTodo) // paintodo에 보낸다
-    saveToDos();
+    const newTodoObj = {
+        text :  newTodo,
+        id : Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj) // paintodo에 보낸다
+    saveTodosToLocalStorage();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) { // 로컬스트리지에 값이 있으면 저장해라
+    const parsedToDos = JSON.parse(savedToDos); // 스트링을 어레이로 변환
+    parsedToDos.forEach(paintToDo); // array의 각 item에 대해 function을 실행
+}
