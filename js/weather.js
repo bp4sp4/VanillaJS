0d4f128f6e051e737aebe6a9ea4a97f0d4168980
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2023-08-15 22:24:25 +0900

    last commit

--- js/weather.js
+++ js/weather.js
@@ -4,10 +4,10 @@ function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
-    .then(Response => Response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
